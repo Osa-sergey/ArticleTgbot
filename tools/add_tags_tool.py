4a9fdc6e287642b5bc8b848dc032fa2c5e5b18ab
Serg Serov <owchin.s@yandex.ru>
2022-07-07 13:45:36 +0300

    Создан скрипт для добавления новых тегов из файла в разметки yaml

---
+++ tools/add_tags_tool.py
@@ -0,0 +1,16 @@
+import yaml
+from yaml.loader import SafeLoader
+
+from tools import *
+from settings import *
+
+if __name__ == '__main__':
+    con = create_connection(db_name, db_user, db_password, db_host, db_port)
+    with open('../scripts/new_tags.yaml') as f:
+        data = yaml.load(f, Loader=SafeLoader)
+    for i in data:
+        for category, v in i.items():
+            for value in v:
+                execute_query(con,
+                              insert_tags_query,
+                              (value, category))
