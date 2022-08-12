da8b3f2c3703b1235e44448c34427a4dbd42f996
Serg Serov <owchin.s@yandex.ru>
2022-08-12 12:08:26 +0300

    Убрал лишние переменные окружения для бота

--- article_tgbot/src/settings/settings.py
+++ article_tgbot/src/settings/settings.py
@@ -2,7 +2,7 @@ import os
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 DB_NAME = os.getenv('DB_NAME')
-DB_USER = os.getenv('DB_USER')
+DB_USER = "postgres"
 DB_PASSWORD = os.getenv('DB_PASSWORD')
 LOCALHOST = "localhost"
 DB_HOST = os.getenv('DB_HOST', LOCALHOST)
@@ -12,5 +12,3 @@ DB_MIN_CON = 5
 DB_MAX_CON = 20
 TAG_FOR_ALL_STUDENTS = os.getenv('TAG_FOR_ALL_STUDENTS')
 LOGGER = "article_tgbot"
-
-
